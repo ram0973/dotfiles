54ecd21a1cf19c17419017cf3c64dc3fcfbe9369
ram0973 <ram0973@gmail.com>
2018-09-11 10:47:19 +0300

    add -y to apt commands

--- ubuntu-glance.sh
+++ ubuntu-glance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install gnome-tweak-tool
-sudo apt-get install gnome-shell-extensions
+sudo apt-get install gnome-tweak-tool -y
+sudo apt-get install gnome-shell-extensions -y
 wget http://drive.noobslab.com/data/Mac/MacBuntu-Wallpapers.zip
 unzip MacBuntu-Wallpapers.zip
 rm MacBuntu-Wallpapers.zip
