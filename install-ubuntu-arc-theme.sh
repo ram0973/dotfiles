cd629a179d5793111e218498108d87bf357a77db
ram0973 <ram0973@gmail.com>
2018-09-11 23:30:22 +0300

    moved to git

--- install-ubuntu-arc-theme.sh
+++ install-ubuntu-arc-theme.sh
@@ -2,12 +2,14 @@
 sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
 rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
 rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
-sudo add-apt-repository ppa:noobslab/themes
-sudo add-apt-repository ppa:noobslab/icons
-sudo apt-get install arc-theme
-sudo apt-get install arc-icons
+git clone https://github.com/NicoHood/arc-theme --depth 1 && cd arc-theme
+./autogen.sh --prefix=/usr
+sudo make install
+git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+./autogen.sh --prefix=/usr
+sudo make install
 wget http://drive.noobslab.com/data/Mac/MacBuntu-Wallpapers.zip
 unzip MacBuntu-Wallpapers.zip
 rm MacBuntu-Wallpapers.zip
-gsettings set org.gnome.desktop.interface gtk-theme "Arc"
-gsettings set org.gnome.desktop.wm.preferences theme "Arc"
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
+gsettings set org.gnome.desktop.wm.preferences theme "Arc-Dark"
