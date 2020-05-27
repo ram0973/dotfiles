62822d92d18342c7a77532a53068688dcf427fed
ram0973 <ram0973@gmail.com>
2020-05-27 15:13:41 +0300

    !

--- install-dotfiles-ubuntu.sh
+++ install-dotfiles-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 # sudo apt full-upgrade
-sudo apt install -y git curl wget neofetch mc net-tools tldr wakeonlan
+sudo apt install -y git curl wget neofetch mc net-tools tldr wakeonlan nmap
 git config --global user.name "ram0973"
 git config --global user.email ram0973@gmail.com
 # Set the cache to timeout after 9 hour (setting is in seconds)
