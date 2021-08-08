14f4065fe594614086223804ec54f2f7f4a50c0c
ram0973 <ram0973@gmail.com>
2021-08-08 20:45:19 +0300

    add comments

--- good-fonts/install-good-fonts.sh
+++ good-fonts/install-good-fonts.sh
@@ -1,3 +1,8 @@
+echo
+echo "This script will make fonts in Ubuntu-based OS look good"
+echo "Make backup of /etc/fonts/local.conf"
+echo "Place ttf fonts to ~/fonts"
+read -rsn1 -p "Press any key to continue or Ctrl+C to quit... "; echo
 sudo cp local.conf /etc/fonts/
 sudo mkdir -p /usr/share/fonts/truetype/msttcorefonts/
 sudo cp ~/fonts/*.ttf  /usr/share/fonts/truetype/msttcorefonts/
