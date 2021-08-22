72ff9ff52446166ec645f5faa4ff7820bfe2a3de
ram0973 <ram0973@gmail.com>
2021-08-22 12:54:19 +0300

    dos2unix to *.sh

--- install-scripts/install-maven.sh
+++ install-scripts/install-maven.sh
@@ -1,2 +1,5 @@
-wget https://mirror.linux-ia64.org/apache/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.zip
-sudo unzip apache-maven-3.8.1-bin.zip -d /opt/maven
+wget https://mirror.linux-ia64.org/apache/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.zip
+sudo unzip apache-maven-3.8.2-bin.zip -d /opt/maven
+# sudo nano /etc/environment
+# add -> /opt/maven/apache-maven-3.8.2/bin
+# . /etc/environment
